8f19cce3960e3270fd053479c634f641a69111ea
Wroyal <91843162+lokjuice@users.noreply.github.com>
2022-05-22 15:32:43 +0300

    First steps

---
+++ src/Server/Server.hpp
@@ -0,0 +1,21 @@
+#ifndef HEAD_HPP
+# define HEAD_HPP
+
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+
+class Server
+{
+private:
+
+public:
+
+};
+
+#endif
---
+++ src/head.hpp
@@ -0,0 +1,12 @@
+#ifndef HEAD_HPP
+# define HEAD_HPP
+
+#include <iostream>
+
+// class Span
+// {
+// private:
+// public:
+// };
+
+#endif
