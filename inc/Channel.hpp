9a7769aac33f0cdf84b80e691700a2676c01d6a1
marie <marievoronova@icloud.com>
2022-06-04 19:22:38 +0300

    Commands are done

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -5,9 +5,21 @@
 
 class Channel {
 private:
+    string  _chnlName;
+    vector<int> _fds;
 
 public:
 
-};
+    Channel();
+    Chanel(string chnlName, int fd);
+    ~Channel();
 
+    //
+    string      ChnlName();
+    vector<int> FdVector();
+    int         FdAdmin();
+
+    //setters
+    void			doChannelPrivmsg(int fd, string message, string nickname, string username);
+};
 #endif
---
+++ inc/Command.hpp
@@ -0,0 +1,48 @@
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+
+#pragma once //Указывает, что компилятор включает файл заголовка только один раз при компиляции файла с исходным кодом.
+#include "GlobalLib.hpp"
+#include "User.hpp"
+#include "Server.hpp"
+// #include "Channel.hpp"
+
+class User;
+class Server;
+class Channel;
+
+class Command{
+    private:
+
+    int     _fd;
+    int     _iter;
+    string  _msg;
+    string  _usrname;
+    string  _nick;
+    string  _cmd;
+
+    vector<User>    _users;
+    vector<string>  _arguments;
+
+    public:
+    Command();
+    Command(string msg, int fd, string nick, vector<User> &users);
+    ~Command();
+
+    //
+    vector<User>    getVectorOfUsers();
+
+    int     commandStart(Server &server, struct pollfd fds[]);
+    void       checkCommand(Server &server, struct pollfd fds[]);
+
+    void QuitCmd(Server &server, struct pollfd fds[]);
+    void NoticeCmd(Server server);
+    void NickCmd(Server &server);
+    void PrivmsgCmd(Server &server);
+
+    //channel cmds in bot
+
+
+
+};
+#endif
